e5f45e434c067eb5d4b291f926cad58f90800f69
Sk Mahafuz Zaman <mahafuzzaman02@gmail.com>
2020-09-06 11:01:27 +0530

    Updates

---
+++ precache-manifest.f3a18eedb6c10334de7015b712d7183a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7abf7846e0ddaac9ab45158a7d54ec3",
+    "url": "/SortingMinions/index.html"
+  },
+  {
+    "revision": "ce2298525d33ba01b402",
+    "url": "/SortingMinions/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2d9c2888c12606ce3cae",
+    "url": "/SortingMinions/static/js/2.329e779e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/SortingMinions/static/js/2.329e779e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce2298525d33ba01b402",
+    "url": "/SortingMinions/static/js/main.cd75c797.chunk.js"
+  },
+  {
+    "revision": "83c8f014c963b417a374",
+    "url": "/SortingMinions/static/js/runtime-main.fd3232e7.js"
+  },
+  {
+    "revision": "e4106c88dd1dc26a3e0e53b2af470bd7",
+    "url": "/SortingMinions/static/media/card_image.e4106c88.png"
+  },
+  {
+    "revision": "efc6e45e41fa81eb4fa1e0ba443452a5",
+    "url": "/SortingMinions/static/media/card_image_five.efc6e45e.png"
+  },
+  {
+    "revision": "bee0dc5951d3b03b7b93872512e0e445",
+    "url": "/SortingMinions/static/media/card_image_four.bee0dc59.png"
+  },
+  {
+    "revision": "bf530bff7f71dc95ced8a72d03bbdcdb",
+    "url": "/SortingMinions/static/media/card_image_three.bf530bff.png"
+  },
+  {
+    "revision": "66ed1b338ba1c335cd9675000d1f8392",
+    "url": "/SortingMinions/static/media/card_image_two.66ed1b33.png"
+  }
+]);
